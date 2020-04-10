177d020a8b775a52a55a5871c5e4d900eb0a41ea
Sara Koceva <sara_koceva@ymail.com>
2020-04-10 19:44:49 +0200

    students class finished

--- Student.java
+++ Student.java
@@ -62,3 +62,35 @@ class Student {
 		
 	}
 }
+
+class facultyFeature {
+List<Student> lista1=new List<Student>();
+String imeFakultet;
+
+public static void Prosek(List<Student>lista) {
+		List<Student>tmp=null;
+		int dolzhina=lista.length();
+		float zbir=0,prosek;
+		for(tmp=lista.getFirst();tmp!=null;tmp=tmp.succ) {
+			zbir+=tmp.element;
+		}
+		prosek=zbir/dolzhina;
+
+		System.out.println(prosek);
+	
+
+public static void BrishiSekojNti(List<Student>lista,int n) {
+		List<Student>p=null;
+		int brojac=1;
+		while(p!=null)
+		{	
+		   if(p!=null && brojac==n)
+	               {		
+	                 lista.delete(p);	
+			 brojac=0;
+		       }
+			p=p.pred;
+			brojac++;
+			
+		}
+}
